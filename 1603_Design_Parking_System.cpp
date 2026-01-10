2013bdc491f5eda5ae5eb8001fb2cbcc809978de
Pratik <pratiksingh111252@gmail.com>
2026-01-10 22:52:07 +0530

    Add LeetCode problem 1603 solution

---
+++ 1603_Design_Parking_System.cpp
@@ -0,0 +1,23 @@
+// LeetCode Problem 1603_Design_Parking_System
+// Status: Accepted
+// Language: C++
+
+class ParkingSystem {
+private:
+    int slots[4] ;
+
+public:
+    ParkingSystem(int big, int medium, int small) {
+        slots[1] = big ;
+        slots[2] = medium ; 
+        slots[3] = small ;
+    }
+
+    bool addCar(int carType) {
+        if (slots[carType] > 0) {
+            slots[carType]-- ;
+            return true ;
+        }
+        return false ;
+    }
+};
