872ff5f0c0126ff25fccbeac24d0d9c0b671d3f3
Pratik <pratiksingh111252@gmail.com>
2026-02-04 23:58:03 +0530

    Add LeetCode problem 680 solution

---
+++ 680_Valid_Palindrome_II.cpp
@@ -0,0 +1,31 @@
+// LeetCode Problem 680_Valid_Palindrome_II
+// Status: Accepted
+// Language: C++
+
+class Solution {
+public:
+
+    bool isPalindrome(string& s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            left++ ;
+            right-- ;
+        }
+        return true ;
+    }
+
+    bool validPalindrome(string s) {
+        int left = 0 ;
+        int right = s.length() - 1 ;
+
+        while (left < right) {
+            if (s[left] != s[right]) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1) ;
+            }
+            left++ ;
+            right-- ;
+        }
+
+        return true ;
+    }
+};
