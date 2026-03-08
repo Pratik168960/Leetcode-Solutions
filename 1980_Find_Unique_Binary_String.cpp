5e5cc08acac77693380322bfe6a66bf49395672e
Pratik Singh <pratiksingh111252@gmail.com>
2026-03-08 23:39:21 +0530

    Add LeetCode problem 1980 solution

---
+++ 1980_Find_Unique_Binary_String.cpp
@@ -0,0 +1,14 @@
+// LeetCode Problem 1980_Find_Unique_Binary_String
+// Status: Accepted
+// Language: C++ 
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string s = "";
+        for (int i = 0; i < nums.size(); i++) {
+            s += nums[i][i] == '0' ? '1' : '0';
+        }
+        return s;
+    }
+};
