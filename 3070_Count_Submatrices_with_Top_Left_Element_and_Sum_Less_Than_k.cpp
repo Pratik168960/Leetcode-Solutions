ad4dd314f8c4cd6170a0628ced630b72c2b4e150
Pratik Singh <pratiksingh111252@gmail.com>
2026-03-18 23:33:11 +0530

    Add LeetCode problem 3070 solution

--- 3070_Count_Submatrices_with_Top_Left_Element_and_Sum_Less_Than_k.cpp
+++ 3070_Count_Submatrices_with_Top_Left_Element_and_Sum_Less_Than_k.cpp
@@ -5,18 +5,19 @@
 class Solution {
 public:
     int countSubmatrices(vector<vector<int>>& grid, int k) {
-        int m = grid.size(), n = grid[0].size(), ans = 0;
+        int m = grid.size(), n = grid[0].size(), ans = 0 ; 
         
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i > 0) grid[i][j] += grid[i-1][j];
-                if (j > 0) grid[i][j] += grid[i][j-1];
-                if (i > 0 && j > 0) grid[i][j] -= grid[i-1][j-1];
+        for (int i = 0 ; i < m ; ++i) {
+            for (int j = 0 ; j < n ; ++j) {
                 
-                if (grid[i][j] <= k) ans++;
+                if (i > 0) grid[i][j] += grid[i-1][j] ;
+                if (j > 0) grid[i][j] += grid[i][j-1] ;
+                if (i > 0 && j > 0) grid[i][j] -= grid[i-1][j-1] ; 
+                
+                if (grid[i][j] <= k) ans++ ;
             }
         }
         
-        return ans;
+        return ans ;
     }
 };
