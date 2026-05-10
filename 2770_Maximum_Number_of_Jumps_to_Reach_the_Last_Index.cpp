88fd671cf7d2fe3638c4ed05a3e542dc82a78902
Pratik Singh <pratiksingh111252@gmail.com>
2026-05-10 23:52:46 +0530

    Add LeetCode problem 2770 solution

---
+++ 2770_Maximum_Number_of_Jumps_to_Reach_the_Last_Index.cpp
@@ -0,0 +1,22 @@
+ // LeetCode Problem 2770_Maximum_Number_of_Jumps_to_Reach_the_Last_Index
+// Status: Accepted
+// Language: C++
+
+class Solution {
+public:
+    int maximumJumps(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        dp[0] = 0;
+        
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (dp[j] != -1 && abs(nums[i] - nums[j]) <= target) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        
+        return dp[n - 1];
+    }
+};
