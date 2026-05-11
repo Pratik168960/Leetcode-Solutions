bca198695eea79d92d13d6411cea9f526f902753
Pratik Singh <pratiksingh111252@gmail.com>
2026-05-11 19:47:20 +0530

    Add LeetCode problem 2553 solution

---
+++ 2553_Separate_the_Digits_in_an_Array.cpp
@@ -0,0 +1,17 @@
+// LeetCode Problem 2553_Separate_the_Digits_in_an_Array
+// Status: Accepted
+// Language: C++
+
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> ans;
+        for (int n : nums) {
+            string s = to_string(n);
+            for (char c : s) {
+                ans.push_back(c - '0');
+            }
+        }
+        return ans;
+    }
+};
