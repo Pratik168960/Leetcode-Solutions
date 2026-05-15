e487fb9ecfe84425c9708778bd45928350df2ba1
Pratik Singh <pratiksingh111252@gmail.com>
2026-05-15 22:41:00 +0530

    Add LeetCode problem 153 solution

---
+++ 153_Find_Minimum_in_Rotated_Sorted_Array.cpp
@@ -0,0 +1,19 @@
+// LeetCode Problem 153_Find_Minimum_in_Rotated_Sorted_Array
+// Status: Accepted
+// Language: C++
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int l = 0, r = nums.size() - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (nums[m] > nums[r]) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
+        }
+        return nums[l];
+    }
+};
