9bd38e4333c43ef2024a554a4d7e11962d910448
Pratik Singh <pratiksingh111252@gmail.com>
2026-06-22 23:09:48 +0530

    Add LeetCode problem 1189 solution

---
+++ 1189_Maximum_Number_of_Balloons.cpp
@@ -0,0 +1,23 @@
+// LeetCode Problem 1189_Maximum_Number_of_Balloons
+// Status: Accepted
+// Language: C++
+
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+       int b = 0, a = 0, l = 0, o = 0, n = 0;
+        
+        for (char c : text) {
+            if (c == 'b') b++;
+            else if (c == 'a') a++;
+            else if (c == 'l') l++;
+            else if (c == 'o') o++;
+            else if (c == 'n') n++;
+        }
+        
+        l = l / 2;
+        o = o / 2;
+        
+        return min({b, a, l, o, n}); 
+    }
+};
