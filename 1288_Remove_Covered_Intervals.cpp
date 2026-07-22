52f3b02f2a442550635c1454de16a5a70f04a9b8
Pratik Singh <pratiksingh111252@gmail.com>
2026-07-22 23:32:48 +0530

    Add LeetCode problem 1288 solution

--- 1288_Remove_Covered_Intervals.cpp
+++ 1288_Remove_Covered_Intervals.cpp
@@ -5,18 +5,17 @@
 class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
-        int removed = 0;
         int n = intervals.size();
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i != j && intervals[j][0] <= intervals[i][0] && intervals[i][1] <= intervals[j][1]) {
-                    removed++;
+        int count = n ;
+        for (int j = 0; j < n; j++){
+            for(int i = 0; i < n; i++){
+                if(intervals[i][0] <= intervals[j][0] && intervals[i][1] >= intervals[j][1]&& (i != j)){
+                    count--;
                     break;
                 }
             }
         }
-
-        return n - removed;
+        
+        return count;
     }
 };
