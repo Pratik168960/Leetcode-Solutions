a976dbbc7692335d196ea709f7dceba79936c882
Pratik Singh <pratiksingh111252@gmail.com>
2026-08-17 00:00:27 +0530

    add Leetcode problem 2029 solution

---
+++ 2029_Stone_Game_IX.cpp
@@ -0,0 +1,21 @@
+// LeetCode Problem 2029_Stone_Game_IX
+// Status: Accepted
+// Language: C++
+
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0, 0, 0};
+        
+        for (int stone : stones) {
+            cnt[stone % 3]++;
+        }
+        
+        if (cnt[0] % 2 == 0) {
+            return cnt[1] > 0 && cnt[2] > 0;
+        } 
+        else {
+            return abs(cnt[1] - cnt[2]) > 2;
+        }
+    }
+};
