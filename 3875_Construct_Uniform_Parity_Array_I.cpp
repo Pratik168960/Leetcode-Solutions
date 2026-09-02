a94b0e7c22b701857528c3da5bdfeea5e1c9d62d
Pratik Singh <pratiksingh111252@gmail.com>
2026-09-02 23:42:10 +0530

    add Leetcode problem 3875 solution

---
+++ 3875_Construct_Uniform_Parity_Array_I.cpp
@@ -0,0 +1,11 @@
+// LeetCode Problem 3875_Construct_Uniform_Parity_Array_I
+// Status: Accepted
+// Language: C++
+
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
